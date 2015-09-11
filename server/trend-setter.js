7bb01b88845dddfdb2552dfa08d5b9d671bc6917
Pramodh <pramodhn91@gmail.com>
2015-09-10 20:12:33 -0700

    stable version for tweet counts

--- server/trend-setter.js
+++ server/trend-setter.js
@@ -4,32 +4,32 @@ var config = require('./config')
 var T = new Twit(config);
 
 /* Get fresh tweets for every 30 seconds */
-var timeout = 30000;
+var timeout = 10000;
 
-function datestring () {
-  var d = new Date(Date.now() - timeout);
-  return d.getUTCFullYear()   + '-'
-     +  (d.getUTCMonth() + 1) + '-'
-     +   d.getDate();
-};
+var maxid = function() {
+  var d = new Date(Date.now());
+  return d.getTime();
+}
 
 setInterval(function() {
 
   /* Parameters for search */
   var params = {
-      q: '#iphonecharger', since: datestring(), count: 100
+      q: '#onedirectionimsorryfor', since_id: maxid, count: 100, result_type:'recent'
   };
 
   /* Get the tweets based on parameters */
   T.get('search/tweets', params, function(err, data, response) {
     if(err) console.log(err);
     var statuses = data.statuses;
-    console.log(statuses.length);
-    console.log(params.since);
 
-    /*for(var i=0; i<statuses.length; i++) {
-        console.log(statuses[i].text);
-    }*/
-
+    if(statuses.length > 0)    {
+      //var md = new Date(statuses[0].created_at);
+      maxid = statuses[0].id;
+      console.log(statuses[0].created_at);
+    }
+    //for(var i=0;i<statuses.length;i++) {
+    //}
+    console.log(statuses.length+ '\n');
   });
 }, timeout);
