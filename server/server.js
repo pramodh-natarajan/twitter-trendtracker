ddd7d03016e961e2e24116bcc462cdebf5112141
Pramodh <pramodhn91@gmail.com>
2015-09-10 23:58:33 -0700

    adding index and server file

---
+++ server/server.js
@@ -0,0 +1,18 @@
+var http = require("http");
+var tweet = require("./trend-tracker");
+
+function start() {
+  tweet.searchTweets();
+
+  function onRequest(request, response) {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    var info = tweet.getTweetInfo();
+    response.write(info);
+    response.end();
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
+}
+
+exports.start = start;
